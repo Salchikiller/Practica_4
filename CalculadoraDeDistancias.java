a4bdd7b43ba6f1169d75f0aed03b84a9a954d2dc
Salchikiller <caralepenafiel@gmail.com>
2024-04-23 00:12:08 +0200

    code with out testing

--- CalculadoraDeDistancias.java
+++ CalculadoraDeDistancias.java
@@ -1,46 +1,52 @@
 public class CalculadoraDeDistancias {
     /**
-* Calcula la distancia de edición, tal y como la expone
-* Jurafsky entre las cadenas ` s1` y ` s2`. */
-public static int calcularDistancia(String s1, String s2){
+     * Calcula la distancia de edición, tal y como la expone
+     * Jurafsky entre las cadenas `s1` y `s2`.
+     */
+    public static int calcularDistancia(String s1, String s2) {
+        int m = s1.length();
+        int n = s2.length();
+        
+        // Creamos una matriz para almacenar los resultados intermedios
+        int[][] dp = new int[m + 1][n + 1];
+        
 
-    int string1 = s1.length();
-    int string2 = s2.length();
+        // i = s1 n
+        // j = s2 m
+        
+        for (int i = 0; i <= m; i++) {
+            for (int j = 0; j <= n; j++) {
+                if (i == 0) {
+                    // Si s1 es una cadena vacía, la distancia es la longitud de s2 (inserciones)
+                    dp[i][j] = j;
+                } else if (j == 0) {
+                    // Si s2 es una cadena vacía, la distancia es la longitud de s1 (eliminaciones)
+                    dp[i][j] = i;
+                } else if (s1.charAt(i-1)== s2.charAt(j-1)){
+                    dp[i][j] = dp[i-1][j-1]; //cuando son igual no cuesta nada 
+
+                } else {
+                    
+                    dp[i][j] = 1 + Math.min(dp[i - 1][j - 1], Math.min(dp[i - 1][j], dp[i][j - 1]));
+                }
+                
+                    
+                    
+                    
+                    
+ 
+                }
+            }
+
+            return dp[n][m];
+
+        }
     
-
     
-     
-    int[][] dp = new int [string1+1][string2+1];
     
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-     return 0; // Este código hay que cambiarlo.
-
-
-
-
-
-
-    } 
-
-
-
-
-
-}
     
+    }
+
+
+
+        
